68c32384a4e5c7366d8e45a6d4f3d3b146b91559
Hima-khalid <Hima-khalid@gmail.com>
2023-06-08 00:33:12 +0300

    lecture monday

--- week 1/project 1.php
+++ week 1/project 1.php
@@ -1,6 +1,28 @@
 <?php
-$name ="Ibrahim";
-gettype($name);
-$str;
-$num=5;
+// string
+$user_name="ali";
+echo $user_name;
+echo "<br>";
+// int 
+$age=20;
+echo $age;
+var_dump($age) ;// information about type && var
+echo "<br>";
+echo gettype($age) ;// return type
+echo "<br>";
+
+// float == double here
+$pi=3.14;
+echo gettype($pi) ;
+echo "<br>";
+//boolean
+$is_admin=true;
+echo gettype($is_admin) ;
+echo "<br>";
+echo $is_admin;// 1
+echo "<br>";
+// object
+// array
+$arr=["ali",2,true];
+var_dump($arr) ;
 ?>
