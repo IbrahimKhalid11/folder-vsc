a5af1e93ca1ed73fd4b408821d3605fe5dadebf9
Hima-khalid <Hima-khalid@gmail.com>
2023-06-10 13:59:44 +0300

    a

---
+++ week 1/assignment/question 1.php
@@ -0,0 +1,2 @@
+<?php
+echo "Hello <br> Ibrahim Khalid";
---
+++ week 1/assignment/question 2.php
@@ -0,0 +1,12 @@
+<?php
+$res1 = -5 + (8 * 6);
+echo $res1;
+echo "<br>";
+$res2 = (55 + 9) % 9;
+echo $res2;
+echo "<br>";
+$res3 = ceil(20 + ((-3 * 5) / 8)); // (ceil) to up if result عشرى
+echo  $res3;
+echo "<br>";
+$res4 = 5 + (((15 / 3) * 2) - (8 % 3));;
+echo  $res4;
